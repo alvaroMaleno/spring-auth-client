c6f2983cfbfa6f945d4ca711a139516bcc42a6ab
alvaromaleno <alvaromalenoalferez@gmail.com>
2024-01-24 17:20:32 +0100

    spring sec client

---
+++ secureui/src/main/java/com/alvaromaleno/secureui/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.alvaromaleno.secureui;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@SuppressWarnings("ALL")
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/");
+    }
+}
---
+++ secureui/src/test/java/com/alvaromaleno/secureui/SecureuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvaromaleno.secureui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
