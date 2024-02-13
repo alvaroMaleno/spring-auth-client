d14b09ad5e81f48c57b81b381cdb5a3178a36cf8
alvaromaleno <alvaromalenoalferez@gmail.com>
2024-02-13 14:41:42 +0100

    adding jwt authentication and report

--- secureui/src/main/java/com/alvaromaleno/secureui/ReportController.java
+++ secureui/src/main/java/com/alvaromaleno/secureui/ReportController.java
@@ -1,11 +1,19 @@
 package com.alvaromaleno.secureui;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 @Controller
 public class ReportController {
+
+    @Autowired
+    private WebClient webClient;
 
     @RequestMapping("/")
     public String loadHome(){
@@ -14,6 +22,17 @@ public class ReportController {
 
     @RequestMapping("/report")
     public String loadReport(Model model){
+
+        Flux<com.alvaromaleno.resourceserver.TollData> response =
+                this.webClient.get()
+                        .uri("http://localhost:8881/api/tolldata")
+                        .retrieve()
+                        .bodyToFlux(com.alvaromaleno.resourceserver.TollData.class);
+
+        List<com.alvaromaleno.resourceserver.TollData> tollData = response.collectList().block();
+
+        model.addAttribute("tolldata", tollData);
+
         return "report";
     }
 }
